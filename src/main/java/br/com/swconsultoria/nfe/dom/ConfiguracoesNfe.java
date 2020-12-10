90d1790ce02040227f20014a4eecbfb8548c4853
Lazaro Fernandes <lazaro.magalhaes@sankhya.com.br>
2020-12-10 09:38:21 -0300

    merge e sincronização com projeto principal so samuel

--- src/main/java/br/com/swconsultoria/nfe/dom/ConfiguracoesNfe.java
+++ src/main/java/br/com/swconsultoria/nfe/dom/ConfiguracoesNfe.java
@@ -9,7 +9,9 @@ import br.com.swconsultoria.nfe.Mock;
 import br.com.swconsultoria.nfe.dom.enuns.AmbienteEnum;
 import br.com.swconsultoria.nfe.dom.enuns.EstadosEnum;
 import br.com.swconsultoria.nfe.util.ConstantesUtil;
+import br.com.swconsultoria.nfe.util.ObjetoUtil;
 
+import java.io.InputStream;
 import java.lang.reflect.Field;
 import java.nio.charset.Charset;
 import java.util.logging.Level;
@@ -43,6 +45,7 @@ public class ConfiguracoesNfe {
     private Mock mockStubs;
     private boolean mockedResult;
     private Integer retry;
+    private InputStream cacert;
 
     /**
      * Este método recebe como parâmetro os dados necessários para iniciar a
@@ -64,6 +67,10 @@ public class ConfiguracoesNfe {
     public static ConfiguracoesNfe criarConfiguracoes(EstadosEnum estado, AmbienteEnum ambiente, Certificado certificado, String pastaSchemas)
             throws CertificadoException {
 
+        ObjetoUtil.verifica(estado).orElseThrow( () -> new IllegalArgumentException("Estado não pode ser Nulo."));
+        ObjetoUtil.verifica(ambiente).orElseThrow( () -> new IllegalArgumentException("Ambiente não pode ser Nulo."));
+        ObjetoUtil.verifica(certificado).orElseThrow( () -> new IllegalArgumentException("Certificado não pode ser Nulo."));
+
         ConfiguracoesNfe configuracoesNfe = new ConfiguracoesNfe();
         configuracoesNfe.setEstado(estado);
         configuracoesNfe.setAmbiente(ambiente);
@@ -83,7 +90,7 @@ public class ConfiguracoesNfe {
         if (Logger.getLogger("").isLoggable(Level.SEVERE)) {
             System.err.println();
             System.err.println("#########################################################");
-            System.err.println("    Api Java Nfe - Versão 4.00.13           ");
+            System.err.println("    Api Java Nfe - Versão 4.00.14-SNAPSHOT (F)           ");
             if (Logger.getLogger("").isLoggable(Level.WARNING)) {
                 System.err.println(" Samuel Olivera - samuel@swconsultoria.com.br ");
             }
@@ -312,4 +319,11 @@ public class ConfiguracoesNfe {
         this.retry = retry;
     }
 
+    public InputStream getCacert() {
+        return cacert;
+    }
+
+    public void setCacert(InputStream cacert) {
+        this.cacert = cacert;
+    }
 }
